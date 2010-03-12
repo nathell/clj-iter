4cff41fd0e5387728c1d4e865fa4addb77c22595
Daniel Janus <dj@danieljanus.pl>
2010-03-12 09:10:44 +0100

    Add project.clj for Leiningen.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clj-iter "0.1.0-SNAPSHOT"
+  :description "An iteration macro inspired by Common Lisp Iterate."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]])
